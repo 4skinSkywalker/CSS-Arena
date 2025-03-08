01642ef0815d83d74fa9af955016da3caedbc07a
Fred's GitHub <stopchemtrailsfred@gmail.com>
2025-03-08 21:06:39 +0100

    load all indexed images

--- battles/battles.js
+++ battles/battles.js
@@ -1,15 +1,16 @@
-function getBattleHtml(battleId, imagePath) {
+function getBattleHtml(battleId, imagePath, belowFold = false) {
     return `<div class="battle">
     <a href="/ring/ring.html?battle=${battleId}"></a>
-    <img src="${imagePath}"
-        alt="Target image">
+    <img src="${imagePath}" ${belowFold ? 'loading="lazy"' : ""} alt="Target image">
 </div>`;
 }
 
 (async function init() {
     const battles = await (await fetch("../battles.json")).json();
     const battlesEl = document.querySelector(".battles");
+    let i = 0;
     for (const [battleId, imagePath] of Object.entries(battles)) {
-        battlesEl.innerHTML += getBattleHtml(battleId, imagePath);
+        i++;
+        battlesEl.innerHTML += getBattleHtml(battleId, imagePath, i > 20);
     }
 })();
